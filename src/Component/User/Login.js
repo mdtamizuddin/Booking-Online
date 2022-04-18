c1eb0402e280b0ad552ebafbe1ace3ea81058c5c
mdtomiz420 <tamizrabbi@gmail.com>
2022-04-18 17:53:00 +0600

    Error And Success Toast Added

--- src/Component/User/Login.js
+++ src/Component/User/Login.js
@@ -1,38 +1,40 @@
-import {  getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useRef, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify';
 import app from '../../firebase.init';
 
 const Login = () => {
     const auth = getAuth(app)
     const emailFeild = useRef('');
     const passwordFeild = useRef('');
-    const [eError , seteError] = useState('')
-    const [success, setSuccess] = useState('')
-   
+
+
     const login = (e) => {
         e.preventDefault()
         const email = emailFeild.current.value;
         const password = passwordFeild.current.value;
 
         signInWithEmailAndPassword(auth, email, password)
-        .then((result) => {
-            setSuccess("Login Success Full")  
-             
-        })
-        .catch((error) => {
-            seteError(error.message)
-        })
+            .then((result) => {
+                const user = result.user.email;
+                const notify = () => toast(user  + " " + "Opend Door For You")
+                notify()
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const notify = () => toast(errorCode)
+                    notify()
+            })
     }
 
     return (
         <form action='submit' onSubmit={login} className='login'>
-            <input  ref={emailFeild} type="email" placeholder='E-mail Adress' required/>
-            <input  ref={passwordFeild} type="password" placeholder='Password' required/>
+            <input ref={emailFeild} type="email" placeholder='E-mail Adress' required />
+            <input ref={passwordFeild} type="password" placeholder='Password' required />
             <button className='btn-login'>Login</button>
             <button className='btn-forget'>Forget Password ?</button>
 
-        <p className='error'>{eError}</p>
-        <p className='text-success'>{success}</p>
+            <ToastContainer />
         </form>
     )
 }
--- src/Component/User/Register.js
+++ src/Component/User/Register.js
@@ -1,64 +1,68 @@
 
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth';
 import React, { useRef, useState } from 'react'
 import app from '../../firebase.init';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Register = () => {
     const emailFeild = useRef('');
     const passwordFeild = useRef('');
     const nameFeild = useRef('');
-    const [eError, seteError] = useState('')
-    const [success, setSuccess] = useState('')
+
     const auth = getAuth(app);
-   
+
     const register = (e) => {
         e.preventDefault()
         const email = emailFeild.current.value;
         const password = passwordFeild.current.value;
         const displayName = nameFeild.current.value;
-        
+
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-           
-            setSuccess("User Create Successfully")
-            updateUserName(displayName)
-            // ...
-        })
-        .catch((error) => {
-            
-            const errorMessage = error.message;
-            seteError(errorMessage)
-          
-        });
-
-
-     
+            .then((userCredential) => {
+              
+                updateUserName(displayName)
+                const notify = () => toast('Sign Up Successfull');
+                notify()
+                emailVerification()
+            })
+            .catch((error) => {
+                const errorMessage = error.code;
+                
+                const notify = () => toast(errorMessage);
+                notify()
+            });
     }
-
-
     const updateUserName = (name) => {
         updateProfile(auth.currentUser, {
             displayName: name,
         })
             .then((result) => {
                 console.log(result)
-               
+
             })
             .catch((error) => {
-               
-               
             })
     }
+    const emailVerification = () => {
+        sendEmailVerification(auth.currentUser)
+            .then(() => {
+                const notify = () => toast('User Verification Mail Sent Plz Check Mail & verify user');
+                notify()
+            });
+    }
+
     return (
-        <form onSubmit={register} className='login'>
-            <input ref={nameFeild} type="text" placeholder='Name' required />
-            <input ref={emailFeild} type="email" placeholder='E-mail Adress' required />
-            <input ref={passwordFeild} type="password" placeholder='Password' required />
-            <button className='btn-login'>Register</button>
-            <p className='error'>{eError}</p>
-            <p className='text-success'>{success}</p>
-        </form>
+        <>
+            <form onSubmit={register} className='login'>
+                <input ref={nameFeild} type="text" placeholder='Name' required />
+                <input ref={emailFeild} type="email" placeholder='E-mail Adress' required />
+                <input ref={passwordFeild} type="password" placeholder='Password' required />
+                <button className='btn-login mb-5'>Register</button>
+
+            </form>
+
+            <ToastContainer />
+        </>
     )
 }
 
